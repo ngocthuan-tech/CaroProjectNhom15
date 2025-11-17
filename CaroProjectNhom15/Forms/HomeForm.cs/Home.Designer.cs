a59de682e82aeae5a67e3b073aef30152a41dd26
ngocthuan-tech <you@example.com>
2025-11-17 16:00:53 +0700

    cập nhật sườn cho Trang chủ

---
+++ CaroProjectNhom15/Forms/HomeForm.cs/Home.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace CaroProjectNhom15.Forms.HomeForm.cs
+{
+    partial class Home
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            label1 = new Label();
+            panel1 = new Panel();
+            button2 = new Button();
+            button3 = new Button();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(37, 124);
+            button1.Name = "button1";
+            button1.Size = new Size(124, 37);
+            button1.TabIndex = 0;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 20.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(313, 28);
+            label1.Name = "label1";
+            label1.Size = new Size(108, 37);
+            label1.TabIndex = 1;
+            label1.Text = "CAPRO";
+            // 
+            // panel1
+            // 
+            panel1.Location = new Point(194, 68);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(594, 370);
+            panel1.TabIndex = 2;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(37, 195);
+            button2.Name = "button2";
+            button2.Size = new Size(124, 37);
+            button2.TabIndex = 3;
+            button2.Text = "button2";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(37, 258);
+            button3.Name = "button3";
+            button3.Size = new Size(124, 37);
+            button3.TabIndex = 4;
+            button3.Text = "button3";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // Home
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(panel1);
+            Controls.Add(label1);
+            Controls.Add(button1);
+            Name = "Home";
+            Text = "Home";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private Label label1;
+        private Panel panel1;
+        private Button button2;
+        private Button button3;
+    }
+}
