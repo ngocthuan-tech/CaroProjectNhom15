244c2acdfd031a4550ed684879151fde5a3d1500
ngocthuan-tech <nguyenngocthuan1210@gmail.com>
2025-12-19 20:20:59 +0700

    Cập nhật UI auth

--- CaroProjectNhom15/Forms/Auth/ForgotPasswordForm.Designer.cs
+++ CaroProjectNhom15/Forms/Auth/ForgotPasswordForm.Designer.cs
@@ -28,12 +28,15 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ForgotPasswordForm));
             Btn_Continue = new Button();
             Lbl_EnterEmail = new Label();
             Tb_Email = new TextBox();
             Lbl_ForgotPassword = new Label();
             LnkL_BackToLogIn = new LinkLabel();
             LnkL_Register = new LinkLabel();
+            pictureBox1 = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
             // 
             // Btn_Continue
@@ -41,11 +44,13 @@
             Btn_Continue.BackColor = Color.Blue;
             Btn_Continue.FlatAppearance.BorderColor = Color.Black;
             Btn_Continue.FlatStyle = FlatStyle.Flat;
-            Btn_Continue.Font = new Font("Segoe UI", 16.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            Btn_Continue.ForeColor = Color.White;
-            Btn_Continue.Location = new Point(245, 249);
+            Btn_Continue.Font = new Font("Snap ITC", 16F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            Btn_Continue.ForeColor = Color.Gold;
+            Btn_Continue.Image = Properties.Resources.giờ_hãy_tạo_một_nền_màu_nâu_đất__đồ_hoạ_2đ;
+            Btn_Continue.Location = new Point(306, 311);
+            Btn_Continue.Margin = new Padding(4, 4, 4, 4);
             Btn_Continue.Name = "Btn_Continue";
-            Btn_Continue.Size = new Size(424, 63);
+            Btn_Continue.Size = new Size(530, 79);
             Btn_Continue.TabIndex = 25;
             Btn_Continue.Text = "Continue";
             Btn_Continue.UseVisualStyleBackColor = false;
@@ -53,10 +58,14 @@
             // Lbl_EnterEmail
             // 
             Lbl_EnterEmail.AutoSize = true;
+            Lbl_EnterEmail.BackColor = Color.Gold;
             Lbl_EnterEmail.Font = new Font("Segoe UI", 13.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            Lbl_EnterEmail.Location = new Point(161, 131);
+            Lbl_EnterEmail.ForeColor = SystemColors.ControlLightLight;
+            Lbl_EnterEmail.Image = Properties.Resources.Gemini_Generated_Image_swcx3cswcx3cswcx;
+            Lbl_EnterEmail.Location = new Point(201, 189);
+            Lbl_EnterEmail.Margin = new Padding(4, 0, 4, 0);
             Lbl_EnterEmail.Name = "Lbl_EnterEmail";
-            Lbl_EnterEmail.Size = new Size(191, 31);
+            Lbl_EnterEmail.Size = new Size(232, 38);
             Lbl_EnterEmail.TabIndex = 24;
             Lbl_EnterEmail.Text = "Enter Your Email: ";
             // 
@@ -64,30 +73,38 @@
             // 
             Tb_Email.BorderStyle = BorderStyle.FixedSingle;
             Tb_Email.Font = new Font("Segoe UI", 15F);
-            Tb_Email.Location = new Point(161, 185);
+            Tb_Email.Location = new Point(201, 231);
+            Tb_Email.Margin = new Padding(4, 4, 4, 4);
             Tb_Email.Name = "Tb_Email";
             Tb_Email.PlaceholderText = "Email";
-            Tb_Email.Size = new Size(585, 41);
+            Tb_Email.Size = new Size(731, 47);
             Tb_Email.TabIndex = 23;
             // 
             // Lbl_ForgotPassword
             // 
             Lbl_ForgotPassword.AutoSize = true;
-            Lbl_ForgotPassword.Font = new Font("Segoe UI", 24F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            Lbl_ForgotPassword.ForeColor = Color.DimGray;
-            Lbl_ForgotPassword.Location = new Point(193, 40);
+            Lbl_ForgotPassword.BackColor = Color.Gold;
+            Lbl_ForgotPassword.Font = new Font("Showcard Gothic", 24F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            Lbl_ForgotPassword.ForeColor = Color.Gold;
+            Lbl_ForgotPassword.Image = Properties.Resources.Gemini_Generated_Image_swcx3cswcx3cswcx;
+            Lbl_ForgotPassword.Location = new Point(241, 50);
+            Lbl_ForgotPassword.Margin = new Padding(4, 0, 4, 0);
             Lbl_ForgotPassword.Name = "Lbl_ForgotPassword";
-            Lbl_ForgotPassword.Size = new Size(530, 54);
+            Lbl_ForgotPassword.Size = new Size(647, 60);
             Lbl_ForgotPassword.TabIndex = 22;
             Lbl_ForgotPassword.Text = "FORGOT YOUR PASSWORD";
             // 
             // LnkL_BackToLogIn
             // 
             LnkL_BackToLogIn.AutoSize = true;
+            LnkL_BackToLogIn.BackColor = Color.Gold;
             LnkL_BackToLogIn.Font = new Font("Segoe UI", 13.8F);
-            LnkL_BackToLogIn.Location = new Point(161, 355);
+            LnkL_BackToLogIn.Image = Properties.Resources.Gemini_Generated_Image_swcx3cswcx3cswcx;
+            LnkL_BackToLogIn.LinkColor = Color.Goldenrod;
+            LnkL_BackToLogIn.Location = new Point(201, 444);
+            LnkL_BackToLogIn.Margin = new Padding(4, 0, 4, 0);
             LnkL_BackToLogIn.Name = "LnkL_BackToLogIn";
-            LnkL_BackToLogIn.Size = new Size(167, 31);
+            LnkL_BackToLogIn.Size = new Size(204, 38);
             LnkL_BackToLogIn.TabIndex = 26;
             LnkL_BackToLogIn.TabStop = true;
             LnkL_BackToLogIn.Text = "Back to Log in?";
@@ -95,27 +112,45 @@
             // LnkL_Register
             // 
             LnkL_Register.AutoSize = true;
+            LnkL_Register.BackColor = Color.Gold;
             LnkL_Register.Font = new Font("Segoe UI", 13.8F);
-            LnkL_Register.Location = new Point(458, 355);
+            LnkL_Register.Image = Properties.Resources.Gemini_Generated_Image_swcx3cswcx3cswcx;
+            LnkL_Register.LinkColor = Color.Goldenrod;
+            LnkL_Register.Location = new Point(572, 444);
+            LnkL_Register.Margin = new Padding(4, 0, 4, 0);
             LnkL_Register.Name = "LnkL_Register";
-            LnkL_Register.Size = new Size(288, 31);
+            LnkL_Register.Size = new Size(352, 38);
             LnkL_Register.TabIndex = 27;
             LnkL_Register.TabStop = true;
             LnkL_Register.Text = "Don't have an account yet?";
             // 
+            // pictureBox1
+            // 
+            pictureBox1.Dock = DockStyle.Fill;
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(0, 0);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(1161, 634);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 28;
+            pictureBox1.TabStop = false;
+            // 
             // ForgotPasswordForm
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(915, 475);
+            ClientSize = new Size(1161, 634);
             Controls.Add(LnkL_Register);
             Controls.Add(LnkL_BackToLogIn);
             Controls.Add(Btn_Continue);
             Controls.Add(Lbl_EnterEmail);
             Controls.Add(Tb_Email);
             Controls.Add(Lbl_ForgotPassword);
+            Controls.Add(pictureBox1);
+            Margin = new Padding(4, 4, 4, 4);
             Name = "ForgotPasswordForm";
             Text = "ForgotPasswordForm";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -128,5 +163,6 @@
         private Label Lbl_ForgotPassword;
         private LinkLabel LnkL_BackToLogIn;
         private LinkLabel LnkL_Register;
+        private PictureBox pictureBox1;
     }
 }
--- CaroProjectNhom15/Properties/Resources.Designer.cs
+++ CaroProjectNhom15/Properties/Resources.Designer.cs
@@ -19,7 +19,7 @@ namespace CaroProjectNhom15.Properties {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "18.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class Resources {
@@ -76,6 +76,16 @@ namespace CaroProjectNhom15.Properties {
         internal static System.Drawing.Bitmap Capro_Game {
             get {
                 object obj = ResourceManager.GetObject("Capro Game", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap Gemini_Generated_Image_swcx3cswcx3cswcx {
+            get {
+                object obj = ResourceManager.GetObject("Gemini_Generated_Image_swcx3cswcx3cswcx", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
