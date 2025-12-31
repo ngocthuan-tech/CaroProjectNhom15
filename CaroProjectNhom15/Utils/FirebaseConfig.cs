586314dd449f6009653be50e5ba107c3ac600870
Tri <thaithanhtri2006@gmail.com>
2025-12-31 23:19:22 +0700

    đổi database + test

--- CaroProjectNhom15/Utils/FirebaseConfig.cs
+++ CaroProjectNhom15/Utils/FirebaseConfig.cs
@@ -6,10 +6,10 @@ namespace CaroProjectNhom15.Utils
     {
         // Thay vì đọc biến môi trường rắc rối, bạn return luôn chuỗi Key của bạn ở đây
         // Bạn lấy Key này trong Project Settings trên web Firebase hoặc trong launchSettings.json cũ của bạn
-        public static string ApiKey => "AIzaSyBfW24uSxwCyHeWUOeaqL51XjqbNTJo-IA";
+        public static string ApiKey => "AIzaSyC8w2KZ_z4JEJ-PnSJe4k4ML9UA6_FhVmc";
 
-        public const string DbURL = "https://project-nhom15-nt106-default-rtdb.asia-southeast1.firebasedatabase.app";
-        public const string AuthDomain = "project-nhom15-nt106.firebaseapp.com";
+        public const string DbURL = "https://carodb-c657f-default-rtdb.asia-southeast1.firebasedatabase.app";
+        public const string AuthDomain = "carodb-c657f.firebaseapp.com";
     }
 }
 
